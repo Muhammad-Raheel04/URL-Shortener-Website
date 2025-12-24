4e5ed722c274f72c301447ca745f338a1e2f8345
Muhammad Raheel <raheel31102004@gmail.com>
2025-12-24 13:39:17 +0500

    updated UX behaviour during api calls

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
 const shortenedURL = document.getElementById("shortenedURL");
+const shortenBtn = document.getElementById('shortenButton');
 
-const shortenBtn = document.getElementById('shortenButton').addEventListener('click', async () => {
+shortenBtn.addEventListener('click', async () => {
 
     const longURLInput = document.getElementById('LongUrlInput').value;
-    if(longURLInput.trim() ===""){
+    if (longURLInput.trim() === "") {
         shortenedURL.innerText = "Please enter a valid URL.";
         shortenedURL.style.color = "#ff0000";
         return;
     }
 
+    shortenBtn.innerText = "Shortening...";
+    shortenBtn.disabled = true;
+    shortenBtn.style.cursor = "not-allowed";
+    shortenBtn.style.opacity = "0.6";
     try {
+
         const response = await fetch("https://api.tinyurl.com/create", {
             method: "POST",
             headers: {
@@ -27,30 +33,36 @@ const shortenBtn = document.getElementById('shortenButton').addEventListener('cl
     } catch (e) {
         shortenedURL.innerText = "Error shortening URL. Please try again.";
     }
+    finally {
+        shortenBtn.innerText = "Shorten URl";
+        shortenBtn.disabled = false;
+        shortenBtn.style.cursor = "pointer";
+        shortenBtn.style.opacity = "1";
+    }
 });
 
 
 const copyBtn = document.getElementById("copyButton");
-copyBtn.addEventListener("click",()=>{
+copyBtn.addEventListener("click", () => {
     const textToCopy = shortenedURL.innerText;
-    if(textToCopy.includes("Error shortening") || textToCopy.includes("appear here")){
+    if (textToCopy.includes("Error shortening") || textToCopy.includes("appear here")) {
         alert("No valid URL to copy!");
         return;
     }
 
     navigator.clipboard.writeText(textToCopy)
-       .then(()=>{
-        alert("Copied to clipboard!");
-       })
-       .catch((err)=>{
-        alert("Failed to copy:");
-       })
+        .then(() => {
+            alert("Copied to clipboard!");
+        })
+        .catch((err) => {
+            alert("Failed to copy:");
+        })
 });
 
 const visitBtn = document.getElementById("visit");
-visitBtn.addEventListener("click",()=>{
+visitBtn.addEventListener("click", () => {
     const urlToVisit = shortenedURL.innerText;
-    if(urlToVisit.includes("Error shortening") || urlToVisit.includes("appear here")){
+    if (urlToVisit.includes("Error shortening") || urlToVisit.includes("appear here")) {
         alert("No valid URL to visit!");
         return;
     }
