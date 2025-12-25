b13dbde9bf642188665cf0b210011cb4c2e50690
Muhammad Raheel <raheel31102004@gmail.com>
2025-12-25 07:31:50 +0500

    removed all the alerts from visit btn

--- script.js
+++ script.js
@@ -73,7 +73,11 @@ const visitBtn = document.getElementById("visit");
 visitBtn.addEventListener("click", () => {
     const urlToVisit = shortenedURL.innerText;
     if (urlToVisit.includes("Error shortening") || urlToVisit.includes("appear here") || urlToVisit.includes("Please enter")) {
-        alert("No valid URL to visit!");
+        // alert("No valid URL to visit!");
+        visitBtn.innerText = "No valid URL to visit";
+        setTimeout(()=>{
+            visitBtn.innerText = "Visit URL";
+        },2000)
         return;
     }
     window.open(urlToVisit, '_blank');
